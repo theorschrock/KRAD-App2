cbd9f23739d8b3543c53a0e0a048c756378fe3ac
theorschrock <Theodore.Schrock@US-CTVGR32.KRONOS.com>
2015-07-16 09:53:36 -0600

    Add Remaining Files

---
+++ source.R
@@ -0,0 +1,208 @@
+library(scales)
+library(shiny)
+library(ReporteRs)
+library(data.table)
+library(stringr)
+library(ggplot2)
+library(ggthemes)
+library(gridExtra)
+library(reshape2)
+library("RCurl")
+library("rjson")
+testimontials<-data.table(read.csv("PBpublicSectorTestimonials.csv",header=T,sep=","))
+references<-data.table(read.csv("PBreferences.csv",header=T,sep=","))
+Dollar<-function(x,r){
+  paste("$",format(round(x,r),big.mark=",",scientific=F),sep="")
+}
+curToNum<-function(x){
+  as.numeric(str_replace_all(str_replace_all(x,"\\$",""),",",""))}
+
+printCurrency <- function(value, currency.sym="$", digits=2, sep=",", decimal=".") {
+  paste(
+    currency.sym,
+    formatC(value, format = "f", big.mark = sep, digits=digits, decimal.mark=decimal),
+    sep=""
+  )
+}
+printPercent<-function(x){
+  paste(round(x*100,2),"%",sep="")
+}
+trim <- function (x) gsub("^\\s+|\\s+$", "", x)
+simpleCap <- function(x) {
+  s <- strsplit(x, " ")[[1]]
+  paste(toupper(substring(s, 1,1)), substring(s, 2),
+        sep="", collapse=" ")
+}
+setClass (Class = "YChartsApiClient",
+          representation = representation(
+            api_key="ANY",
+            base_url="ANY",
+            funct = "function"
+          ),
+          prototype = list ( api_key="", base_url="https://ycharts.com/api/v2/",
+                             funct = function (.Object) { print (.Object) }   ),
+)
+
+setMethod ('initialize', 'YChartsApiClient',
+           function(.Object, api_key='', base_url='') {
+             if(nargs() > 1) .Object@api_key  = api_key
+             if(nargs() > 2) .Object@base_url = base_url
+             .Object
+           })
+
+
+setGeneric("fetch_data",  function(.Object, y_url, params="") standardGeneric("fetch_data"))
+setMethod("fetch_data",  "YChartsApiClient",
+          function(.Object, y_url, params='') {
+            if ( params != "" ) y_url = paste(y_url, params, sep='', collapse='')
+            headers=list('X-YCHARTSAUTHORIZATION'=.Object@api_key)
+            print (headers)
+            return (getURL ( y_url, httpheader = headers,.opts = list(ssl.verifypeer = FALSE) ))
+          })
+
+setGeneric("verify_list_or_string", function(.Object, imp_data) standardGeneric("verify_list_or_string"))
+setMethod("verify_list_or_string", "YChartsApiClient",
+          function(.Object, imp_data) {
+            if ( class(imp_data) == 'character' ) return (imp_data)
+            if ( class(imp_data) == 'list' ) return (paste(imp_data, sep=',', collapse=','))
+            return ("not_a_string_passed")
+          })
+
+setGeneric("verify_date", function(.Object, imp_data) standardGeneric("verify_date"))
+setMethod("verify_date", "YChartsApiClient",
+          function(.Object, imp_data) {
+            if ( ( class(imp_data) == 'POSIXct' ) || ( class(imp_data) == 'POSIXlt')) {  return ( strftime (imp_data, "%Y-%m-%d")) }
+            if ( class(imp_data) == 'character' || class(imp_data)=='numeric' ) return ( imp_data )
+            return ("")
+          })
+
+
+# Arguments:
+
+#   companies (string or array of strings): The ticker (+ exchange
+#     symbol for non-US companies) of companies for which we want info.
+#   	eg. ['AAPL', 'GOOG'] or 'AAPL'
+
+# Returns an array of information about the requested company/companies including
+# things like the company name, exchange, profile, industry, etc.
+
+
+setGeneric("get_company_info", function(.Object, companies) standardGeneric("get_company_info"))
+setMethod("get_company_info", "YChartsApiClient",
+          function(.Object, companies) {
+            info_url = 'companies/'
+            companies = verify_list_or_string(.Object, companies)
+            y_url = paste (.Object@base_url, paste(info_url, companies, sep='/', collapse=NULL), sep='', collapse=NULL)
+            json_data = fetch_data(.Object, y_url)
+            #return (json_data);
+            return (fromJSON(json_data))
+          })
+
+setGeneric("get_industry_info", function(.Object, industries) standardGeneric("get_industry_info"))
+setMethod("get_industry_info", "YChartsApiClient",
+          function(.Object, industries) {
+            info_url = 'classifications/USA/industries/'
+            companies = verify_list_or_string(.Object, industries)
+            y_url = paste (.Object@base_url, paste(info_url, industries, sep='/', collapse=NULL), sep='', collapse=NULL)
+            json_data = fetch_data(.Object, y_url)
+            #return (json_data);
+            return (fromJSON(json_data))
+          })
+
+#Arguments:
+#
+#	companies (string or array of strings): The ticker (+ exchange
+#		symbol for non-US companies) of companies for which we want info.
+#		eg. ['AAPL', 'GOOG'] or 'AAPL'
+#
+#	metrics (string or array of strings): the financial metrics we want to
+#		return.  Eg. 'net_income' or ['net_income', 'revenues']
+#
+#	date_attr (string or integer): the date for which we want
+#		data points.  If there is no data point on that date, we will return
+#		the first observed datapoint before that date.
+#		String must be of the form YYYY-MM-DD
+#		Integers will look back from today's date (eg. -1 will look back
+#			one day for daily data, one quarter for quarterly data, etc.)
+#
+#Returns an array with the requested information.
+
+
+setGeneric("get_company_data_point", function(.Object, companies, metrics, date_attr='' ) standardGeneric("get_company_data_point"))
+setMethod("get_company_data_point", "YChartsApiClient",
+          function(.Object, companies, metrics, date_attr='') {
+            point_url='companies/%s/points/%s'
+            companies = verify_list_or_string(.Object, companies)
+            metrics = verify_list_or_string(.Object, metrics)
+            
+            y_url = paste (.Object@base_url, paste('companies', companies, 'points', metrics, sep='/', collapse=NULL), sep='', collapse=NULL)
+            if (date_attr != '' ) {
+              params = URLencode( paste('?date=', verify_date(.Object, date_attr), sep='', collapse=NULL) )
+            } else {
+              params = ''
+            }
+            
+            #print(y_url)
+            
+            json_data = fetch_data(.Object, y_url, params)
+            
+            #return (json_data);
+            return (fromJSON(json_data))
+          })
+
+
+
+#Arguments:
+#
+#	companies (string or array of strings): The ticker (+ exchange
+#		symbol for non-US companies) of companies for which we want info.
+#		eg. ['AAPL', 'GOOG'] or 'AAPL'
+#
+#	metrics (string or array of strings): the financial metrics we want to
+#		return.  Eg. 'net_income' or ['net_income', 'revenues']
+#
+#	start_date (string or integer): the start date for which
+#		we want data points.
+#		String must be of the form YYYY-MM-DD or -I
+#		Integers will look back from today's date (eg. -1 will look back
+#			one day for daily data, one quarter for quarterly data, etc.)
+#
+#	end_date (string): the end date for which
+#		we want data points.
+#
+#Returns an array with the requested information.
+
+setGeneric("get_company_data_timeseries", function(.Object, companies, metrics, start_date='', end_date='') standardGeneric("get_company_data_timeseries"))
+setMethod("get_company_data_timeseries", "YChartsApiClient",
+          function(.Object, companies, metrics, start_date='', end_date='') {
+            
+            companies = verify_list_or_string( .Object, companies )
+            metrics = verify_list_or_string( .Object, metrics )
+            y_url = paste (.Object@base_url, 'companies', companies, 'series', metrics, sep='/', collapse=NULL)
+            
+            params = URLencode ( paste('?start_date=', verify_date(.Object, start_date), '&', 'end_date=', verify_date(.Object, end_date), sep='', collapse=NULL ))
+            
+            json_data = fetch_data( .Object, y_url, params )
+            
+            
+            #return (json_data);
+            return (fromJSON( json_data ))
+          })
+
+
+
+
+
+
+
+
+
+
+# Sample Code Using the API Client
+
+FN<-function(x) {
+  as.numeric(as.character(x))
+}
+sac <- new ("YChartsApiClient", api_key="NWh0HNOObqf20CO3boO8NQ") # Enter the Key here. See http://ycharts.com/accounts/my_account
+
+
---
+++ ui.R
@@ -0,0 +1,178 @@
+
+# This is the user-interface definition of a Shiny web application.
+# You can find out more about building applications with Shiny here:
+#
+# http://shiny.rstudio.com
+#
+
+source("source.R")
+
+shinyUI(
+  navbarPage("KRAD",
+             tabPanel("Prospecting Brief",
+                      
+                      # Sidebar with a slider input for number of bins
+                      sidebarLayout(
+                        sidebarPanel(
+                          textInput("orgName","Organization",value=NULL),
+                          selectizeInput('sVertical',"Sub-Vertical",choices=c("K-12","Municipal Government","State Government","Higher Ed"),
+                                         selected="Municipal Government",multiple=F),
+                          numericInput("nEE","Total Number of Employees",value=NULL),
+                          uiOutput("eeSplitCheckbox"),
+                          conditionalPanel(
+                            condition="input.eeSplit==true",
+                            uiOutput("nEEexemptui"),
+                            uiOutput("nEEnonExemptui")
+                          ),
+                          conditionalPanel(
+                            condition="input.eeSplit==false",
+                            uiOutput("eeSliderui"),
+                            h5(strong("Total Number of Exempt Employees")),
+                            uiOutput("nEEexemptText"),
+                            h5(strong("Total Number of Non-Exempt Employees")),
+                            uiOutput("nEEnonExemptText")
+                          ),
+                          numericInput("annualPayroll","Annual Payroll",value=NULL),
+                          uiOutput("paySplitCheckbox"),
+                          conditionalPanel(
+                            condition="input.paySplit==true",
+                            uiOutput("payExemptui"),
+                            uiOutput("payNonExemptui")
+                          ),
+                          conditionalPanel(
+                            condition="input.paySplit==false",
+                            uiOutput("paySliderui"),
+                            h5(strong("Annual Exempt Payroll")),
+                            uiOutput("payExemptText"),
+                            h5(strong("Annual Non-Exempt Payroll")),
+                            uiOutput("payNonExemptText")
+                          ),
+                          uiOutput("overtimeCheckbox"),
+                          conditionalPanel(
+                            condition="input.OT==false",
+                            uiOutput("overtimeSliderui"),
+                            h5(strong("Annual Overtime Estimate")),
+                            uiOutput("overtimeText")
+                          ),
+                          conditionalPanel(
+                            condition="input.OT==true",
+                            numericInput("OTamount","Annual Overtime",value=NULL)
+                          )
+                        ),
+                        
+                        # Show a plot of the generated distribution
+                        mainPanel(
+                          h3("Metrics"),
+                          tableOutput("table"),
+                          h3("Data Quality Check"),
+                          tableOutput("dataQuality"),
+                          h3("Value Areas"),
+                          fluidRow(
+                            column(3,
+                                   checkboxInput("otsavings","Overtime Reduction",value=T)
+                            ),
+                            column(3,
+                                   checkboxInput("payrollInflation","Payroll Inflation",value=T)
+                            ),
+                            column(3,
+                                   checkboxInput("leaveInflation","Leave Inflation",value=T)
+                            )
+                          ),
+                          fluidRow(
+                            column(3,
+                                   checkboxInput("absenteeism","Unscheduled Absenteeism",value=T)
+                            ),
+                            column(3,
+                                   checkboxInput("calculationError","Calculation Error",value=T)
+                            ),
+                            column(3,
+                                   checkboxInput("flsa","FLSA/FMLA Compliance",value=T)
+                            )),
+                          fluidRow(
+                            conditionalPanel(
+                              condition="input.otsavings==true",
+                              h3("Reduction in Controllable Overtime"),   
+                              uiOutput("otSavingsSlider"),
+                              tableOutput("otLook")),
+                            conditionalPanel(
+                              condition="input.payrollInflation==true",
+                              h3("Payroll Inflation Due to Time Stamp"),
+                              sliderInput("percEEextraPay","Percentage of Employees Recieving Extra Pay",value=.12,min=0,max=.5,step=.01),
+                              sliderInput("minGamedPerWeek","Minutes Gamed Per Week",value=60,min=0,max=150,step=1),
+                              tableOutput("piLook")
+                            ),
+                            conditionalPanel(
+                              condition="input.leaveInflation==true",
+                              h3("Leave Inflation Reduction"),
+                              sliderInput("PTOhours","Number of unreported PTO hours per year per employee",value=10,min=0,max=30,step=1),
+                              sliderInput("kronosPTOEstimate","Kronos Estimate of Underreporting Employees",value=.25,min=0,max=1,step=.01),
+                              tableOutput("leLook")
+                            ),
+                            conditionalPanel(
+                              condition="input.absenteeism==true",
+                              h3("Reduction in Unscheduled Absenteeism Costs"),
+                              sliderInput("absenceRate","Average total costs of incidental unplanned absences(% of payroll)",value=.06,min=0,max=.2,step=.01),
+                              sliderInput("kronosAbsenceImpact","Kronos Ability to Impact",value=.05,min=0,max=.25,step=.01),
+                              tableOutput("abLook")
+                            ),
+                            conditionalPanel(
+                              condition="input.calculationError==true",
+                              h3("Reduction in Calculation Error"),
+                              sliderInput("payrollErrorRate","Annual Payroll Error Rate",value=.012,min=0,max=.04,step=.001),
+                              sliderInput("kronosCalcErrorImpact","Kronos Ability to Impact",value=.80,min=0,max=1,step=.01),
+                              tableOutput("ceLook")
+                            ),
+                            actionButton("buildPB","Generate Document"),
+                            hr()
+                          )
+                          
+                        )
+                      )
+             ),
+             tabPanel("ACA Brief",
+                      fluidRow(
+                        column(6,
+                               textInput("ACAorgName","Organization Name:"),
+                               textInput("subvertACA","Sub-Vertical:"),
+                               textInput("date","Date:")),
+                        column(6,
+                               textInput("repName","Rep Name:"),
+                               textInput("repEmail","Rep Email:"),
+                               textInput("repNumber","Rep Number:")
+                        )
+                      ),
+                      hr(),
+                      numericInput("acaNumEETotal","Total Number of Employees",value=NULL),
+                      numericInput("acaFTEETotal","Total FT",value=NULL),
+                      numericInput("acaPTEETotal","Total PT",value=NULL),
+                      numericInput("BenEx","Estimated Benefit Expense",value=10000),
+                      numericInput("COLA","Estimated Cost of Living Adjustment",value=.02),
+                      uiOutput("genACA")
+             ),
+             tabPanel("Financial Profile",
+                      sidebarLayout(
+                        sidebarPanel(
+                          selectizeInput("company","Company Ticker",choices=NULL,selected=NULL,multiple=F,
+                                         options = list(create = TRUE)),
+                          selectizeInput('competitors',"Competitor Tickers",choices=NULL,selected=NULL,multiple=T,
+                                         options = list(create = TRUE)),
+                          numericInput("years","Years to Analyze",value=4,min=1),
+                          selectizeInput('metricSet',"Metric Set",choices=
+                                           c("Profitability","Employee Metrics"),
+                                         selected=NULL,multiple=F),
+                          uiOutput("metrics"),
+                          actionButton("addToDR","Add Graph to Profile"),
+                          actionButton("generateReport","Generate Profile")
+                        ),
+                        
+                        # Show a plot of the generated distribution
+                        mainPanel(
+                          h3(textOutput("graphHeader")),
+                          plotOutput("plot"),
+                          textOutput("sentance1"),
+                          br(),
+                          textOutput("sentance2")
+                        )
+                      )
+             )
+  ))
